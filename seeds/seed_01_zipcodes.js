08c0d70a93b489f25eda5875816dc7da1e9174d6
SteveODell <saodell@mac.com>
2017-03-22 22:35:28 -0600

    added some lats longs for bookkeeping

--- seeds/seed_01_zipcodes.js
+++ seeds/seed_01_zipcodes.js
@@ -2,15 +2,15 @@ exports.seed = (knex, Promise) => {
   return knex('zipcode').del()
     .then( () => {
       return knex('zipcode').insert([
-        {id: 80302}, //Boulder
-        {id: 81657}, //Vail
-        {id: 81620}, //Beaver Creek
-        {id: 80809}, //Pikes Peak
-        {id: 80424}, //Breckenridge
-        {id: 80435}, //Keystone
-        {id: 80025}, //Eldorado Springs
-        {id: 80466}, //Nederland
-        {id: 80465} //Red Rocks
+        {id: 80302}, //Boulder, "lat": 40.038629, "lng": -105.3716684
+        {id: 81657}, //Vail, lat": 39.6648024, "lng": -106.3934777
+        {id: 81620}, //Beaver Creek "lat": 39.595736, "lng": -106.5230542
+        {id: 80809}, //Pikes Peak  "lat": 38.8408707, "lng": -105.0422595
+        {id: 80424}, //Breckenridge  "lat": 39.476845, "lng": -106.0169971
+        {id: 80435}, //Keystone  "lat": 39.5791675, "lng": -105.9347384
+        {id: 80025}, //Eldorado Springs  "lat": 39.9324862, "lng": -105.2769348
+        {id: 80466}, //Nederland "lat": 39.9613759, "lng": -105.5108312
+        {id: 80465} //Red Rocks  "lat": 39.664879, "lng": -105.2051438
       ])
     })
 }
